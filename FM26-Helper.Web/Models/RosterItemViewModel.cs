cda856b350981f37903a247349a661c1cb3f71fc
Jason Nash <JasonRNash@gmail.com>
2025-12-10 14:36:30 -0600

    feat(ui): implement horizontal scrolling and sticky columns (closes #7)

--- FM26-Helper.Web/Models/RosterItemViewModel.cs
+++ FM26-Helper.Web/Models/RosterItemViewModel.cs
@@ -16,5 +16,23 @@ namespace FM26_Helper.Web.Models
         
         public string BestOutPossessionRole { get; set; } = string.Empty;
         public double BestOutPossessionScore { get; set; }
+
+        // Physical
+        public int Pace { get; set; }
+        public int Acceleration { get; set; }
+        public int Stamina { get; set; }
+        public int Strength { get; set; }
+
+        // Technical
+        public int Finishing { get; set; }
+        public int Dribbling { get; set; }
+        public int Passing { get; set; }
+        public int Technique { get; set; }
+
+        // Mental
+        public int Composure { get; set; }
+        public int Decisions { get; set; }
+        public int Vision { get; set; }
+        public int WorkRate { get; set; }
     }
 }
